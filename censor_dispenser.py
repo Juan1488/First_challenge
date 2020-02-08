89c07decbb9b4fb6fc3100157f7f3013840ab36e
Sicrow <juanrivas260196@gmail.com>
2020-02-08 14:28:45 -0500

    Added logic for function to get number of istances of phrases or words in a list

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,12 +3,24 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 
 #function that finds how many instances the word appears in the email
 def censor_string(word, string):
     word_count = string.count(word)
-    return f"{word} was found {word_count} times in this email"
+    return f"\"{word}\" was found {word_count} times in this email"
 
 #uncomment to test censor_string fucntion
 #test = censor_string("learning algorithms", email_one)
 #print(test)
+
+#Finds all instances proprietary terms appear in the email
+def censor_string_from_list(list, string):
+    word_count_complete = ""
+    for str in list:
+        word_count = string.count(str)
+        word_count_complete += f"\"{str}\" was found {word_count} time(s) in this email\n"
+    return word_count_complete
+
+#uncomment to test censor_string_from_lsit fucntion
+#print(censor_string_from_list(proprietary_terms, email_two))
