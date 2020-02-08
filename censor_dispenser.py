d715d41af578443a19b52dded2bc38d02caf4bac
Sicrow <juanrivas260196@gmail.com>
2020-02-08 17:18:53 -0500

    fixed logic in censor_string and censor_string_from_list to censor words and phrases

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -1,26 +1,30 @@
 # These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
+import re
+
 email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
-#function that finds how many instances the word appears in the email
+#function that finds how many instances the word appears in the email and censors it.
 def censor_string(word, string):
-    word_count = string.count(word)
-    return f"\"{word}\" was found {word_count} times in this email"
+    censored_string = string.replace(word, ("X"*len(word)))
+    censored_string = string.replace(word.title(), ("X"*len(word)))
+    censored_string = string.replace(word.upper(), ("X"*len(word)))
+    return censored_string
 
 #uncomment to test censor_string fucntion
 #test = censor_string("learning algorithms", email_one)
 #print(test)
 
-# function finds all instances proprietary terms appear in the email
+# function finds all instances proprietary terms appear in the email and censors it.
 def censor_string_from_list(list, string):
-    word_count_complete = ""
     for str in list:
-        word_count = string.count(str)
-        word_count_complete += f"\"{str}\" was found {word_count} time(s) in this email.\n"
-    return word_count_complete
+        string = string.replace((" " + str), (" " + "X" * len(str)))
+        string = string.replace((" " + str.title()), (" " + "X" * len(str)))
+        string = string.replace((" " + str.upper()), (" " + "X" * len(str)))
+    return string
 
 #uncomment to test censor_string_from_lsit fucntion
 #print(censor_string_from_list(proprietary_terms, email_two))
